dc25e512a2e024fbc5bb4e68a90e37283e111431
Rafael4ndrade <1409rafaelasm@gmail.com>
2021-09-17 21:27:55 -0300

    Aula de versionamento impar e par

---
+++ Aula1/Par e Impar.js
@@ -0,0 +1,19 @@
+var r = 0
+
+var i = 1;
+console.log("Este é número é zero")
+
+for(var i = 1; i<11; i++){
+    if (i % 2 == 0)
+    {
+       console.log("Este número é par",  + i)
+    }
+else if (i % 2 !== 0)
+    {
+        console.log("Este número é impar",  + i)
+    }
+
+}
+
+
+
